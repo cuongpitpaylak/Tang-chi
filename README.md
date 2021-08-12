9a4463dce337507bd191597e78bca97f853c20cc
cuong132 <67764489+cuong132@users.noreply.github.com>
2021-08-12 07:32:24 +0700

    upload

---
+++ README.md
@@ -0,0 +1,1 @@
+#cuongpitpaylak.chaucuong.github.io
---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee luu chị!',
+    introDesc: `Em có điều này muốn hỏi chị, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oke',
+    title: 'Chị yêu em có phải không 😙',
+    desc: 'Chị không trả lời hoặc thoát ra, tức là Chị là vợ em đó nha :> ',
+    btnYes: 'yêu ơi là iuuu <33',
+    btnNo: 'Em mơ à :333',
+    question:'Nói cho tới lý do vì sao Chị yêu em đii <3',
+    btnReply: 'Gửi cho em <3',
+    reply: 'Tại vì em đẹp trai hihi <33333333',
+    mess: 'Em biết mà 🥰. Love you 300.000 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho em đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/cuongpitpaylak/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
